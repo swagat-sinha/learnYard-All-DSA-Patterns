baff3c7a61f21ac1af8105c9a0b25fd4819aff69
Swagat Sinha <swagatcool.sinha3@gmail.com>
2025-01-06 23:39:10 +0530

    Update BalancedBinaryTree efficient code - O(n) approach

--- src/main/java/com/dsa/binaryTress/propertiesOfTrees/BalancedBinaryTree.java
+++ src/main/java/com/dsa/binaryTress/propertiesOfTrees/BalancedBinaryTree.java
@@ -2,6 +2,16 @@ package com.dsa.binaryTress.propertiesOfTrees;
 
 import com.dsa.binaryTress.TreeNode;
 
+class TreeInfo{
+int height;
+boolean isBalanced;
+
+TreeInfo(int height , boolean isBalanced){
+this.height = height;
+this.isBalanced = isBalanced;
+}
+}
+
 public class BalancedBinaryTree {
 
     //time -> O(n^2)
@@ -29,4 +39,25 @@ public class BalancedBinaryTree {
         Integer heightOfRightSubtree  = heightOfTree(root.right);
         return Math.max(heightOfRightSubtree , heightOfLeftSubtree) + 1;
     }
+
+    // T=> O(n)
+    public boolean isBalancedBinaryTreeEfficient(TreeNode root){
+
+        return isBalancedBinaryTreeEfficientHelper(root).isBalanced;
+    }
+
+    private TreeInfo isBalancedBinaryTreeEfficientHelper(TreeNode root){
+
+        if(root == null){
+            return new TreeInfo(0,true);
+        }
+
+        TreeInfo leftSubtree = isBalancedBinaryTreeEfficientHelper(root.left);
+        TreeInfo rightSubtree = isBalancedBinaryTreeEfficientHelper(root.right);
+
+        boolean isBalanced = leftSubtree.isBalanced && rightSubtree.isBalanced && (Math.abs(leftSubtree.height- rightSubtree.height)<=1);
+        int height = Math.max(leftSubtree.height, rightSubtree.height) +1;
+        return new TreeInfo(height,isBalanced);
+    }
+
 }
